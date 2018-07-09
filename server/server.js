12adc046414f12d7748766bfddf85f6ac5808d5c
Kevin Montano <kmont5@uis.edu>
2018-07-08 20:12:37 -0500

    Added small socket.io functionality

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 var app = express();
-
 const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected user');
+  });
+})
+
+server.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 

f0b6ceabb27a79744314072b06a053d7ee303645
Kevin Montano <kmont5@uis.edu>
2018-07-10 15:46:13 -0500

    Testing scripts and refactor for generateMessage added

--- server/server.js
+++ server/server.js
@@ -3,51 +3,37 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New User Connected');
+  console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message) => {
-    console.log('createMessage: ', message);
-
-    // io.emit('newMessage', {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
-    socket.broadcast.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
   });
 
   socket.on('disconnect', () => {
-    console.log('Disconnected user');
+    console.log('User was disconnected');
   });
-})
-
-server.listen(port, () => {
-  console.log(`Started up at port ${port}`);
 });
 
-module.exports = {app};
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
